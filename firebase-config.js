17671d909eb443e65e7070473ae3cce46eefc681
melyymix1 <mgutierrezs2104@alumno.ipn.mx>
2024-04-30 01:19:33 -0600

    subo codigo del movil

---
+++ firebase-config.js
@@ -0,0 +1,27 @@
+
+export const firebaseConfig = {
+ /* apiKey: "AIzaSyBw_d1q_5PvrmN4uPHCHGOjmHVXCd43NeU",
+  authDomain: "bachecito-26-3.firebaseapp.com",
+  projectId: "bachecito-26-3",
+  storageBucket: "bachecito-26-3.appspot.com",
+  messagingSenderId: "579834044731",
+  appId: "1:579834044731:web:eda3bcee5213884b6e171e"*/
+ 
+  /*apiKey: "AIzaSyDRgcJ0DpDxYXlbvjB0kNa6-IMcMI6UPS0",
+
+  authDomain: "web---1.firebaseapp.com",
+
+  projectId: "web---1",
+
+  storageBucket: "web---1.appspot.com",
+
+  messagingSenderId: "493674989659",
+
+  appId: "1:493674989659:web:7f7f59c2d7418db6ff177e"*/
+  apiKey: "AIzaSyDwzBnKRng7nqMCOfUp0cYlweR8XE49ZLk",
+  authDomain: "si-la-mami.firebaseapp.com",
+  projectId: "si-la-mami",
+  storageBucket: "si-la-mami.appspot.com",
+  messagingSenderId: "419456667923",
+  appId: "1:419456667923:web:a9fdfafa78db42eaa0cae6"
+}
